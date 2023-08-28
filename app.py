451cd3214dd76b0ba7daf3db4d0b8da585641c0c
Oliver <vuoreol@pm.me>
2023-08-28 15:10:55 +0000

    Added some things

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, make_response
 
 app = Flask(__name__)
-
-
-
 
 events_data = [
     {
@@ -21,22 +18,44 @@ events_data = [
     # Add more events
 ]
 
+def get_template_folder(lang):
+    if lang == 'fi':
+        return 'fi'
+    
+    return 'en'
+
+def set_language_cookie(lang):
+    response = make_response(redirect(request.referrer or '/'))
+    response.set_cookie('language', lang)
+    return response
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="Your Movement Name", current_year=2023)
+    lang = request.cookies.get('language', request.accept_languages.best_match(['fi', 'en']))
+    template_folder = get_template_folder(lang)
+    return render_template(f'{template_folder}/index.html', title="Your Movement Name", current_year=2023)
 
 @app.route('/events')
 def events():
-    return render_template('events.html', title='Events', events=events_data, current_year=2023)
+    lang = request.cookies.get('language', request.accept_languages.best_match(['fi', 'en']))
+    template_folder = get_template_folder(lang)
+    return render_template(f'{template_folder}/events.html', title='Events', events=events_data, current_year=2023)
 
 @app.route('/about')
 def about():
-    return render_template('about.html', title='About Us')
+    lang = request.cookies.get('language', request.accept_languages.best_match(['fi', 'en']))
+    template_folder = get_template_folder(lang)
+    return render_template(f'{template_folder}/about.html', title='About Us')
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html', title="Contact Us", current_year=2023)
+    lang = request.cookies.get('language', request.accept_languages.best_match(['fi', 'en']))
+    template_folder = get_template_folder(lang)
+    return render_template(f'{template_folder}/contact.html', title="Contact Us", current_year=2023)
+
+@app.route('/change_language/<lang>')
+def change_language(lang):
+    return set_language_cookie(lang)
 
 if __name__ == '__main__':
     app.run(debug=True)
