6927b39817b0784eb58b7657939f2aa78478ec85
Oliver <vuoreol@pm.me>
2023-08-28 11:20:15 +0000

    Added basic stuff...

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+events_data = [
+    {
+        'title': 'Event 1',
+        'date': 'September 10, 2023',
+        'location': 'Virtual or Physical Location',
+        'description': 'Description of Event 1...'
+    },
+    {
+        'title': 'Event 2',
+        'date': 'September 15, 2023',
+        'location': 'Virtual or Physical Location',
+        'description': 'Description of Event 2...'
+    }
+    # Add more events
+]
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', title="Your Movement Name", current_year=2023)
+
+@app.route('/events')
+def events():
+    return render_template('events.html', title='Events', events=events_data, current_year=2023)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About Us')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', title="Contact Us", current_year=2023)
+
+if __name__ == '__main__':
+    app.run(debug=True)
