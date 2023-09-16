1b716773464eba5a8c71a33cf650bbb4982d4924
Verso <vuoreol@pm.me>
2023-09-16 12:23:28 +0000

    Fixed languages, added info

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ from bson import ObjectId  # Import ObjectId class
 from flask_sitemap import Sitemap
 import json
 
-from photo import photo_bp
-
 with open("config.json", "r") as f:
     config = json.load(f)
 
@@ -15,9 +13,6 @@ app = Flask(__name__)
 app.secret_key = 'your_secret_key_here'
 app.config['SITEMAP_INCLUDE_RULES_WITHOUT_PARAMS'] = True
 sitemap = Sitemap(app=app)
-
-# Register the Blueprint
-app.register_blueprint(photo_bp)
 
 # Set up MongoDB connection
 client = MongoClient(config["mongodb"]["uri"])
@@ -29,18 +24,20 @@ joins_collection = db["joins"]
 signups_collection = db["signups"]
 
 
-
 @app.before_request
 def set_template_folder():
     visits_collection.insert_one({"ip": request.headers.get(
         'X-Forwarded-For', request.remote_addr), "lang": "unknown", "time": datetime.now()})
 
 # Serve robots.txt
+
+
 @app.route('/robots.txt', methods=['GET'])
 def robots_txt():
     content = "User-agent: *\nDisallow:"
     response = Response(content, content_type='text/plain')
     return response
+
 
 @app.route("/<lang>/")
 @app.route('/')
@@ -52,22 +49,26 @@ def index(lang="fi"):
 
 # EVERYTHING REGARDING EVENTS
 
+
 def get_event_date(event):
     date_str = event['date']
     return datetime.strptime(date_str, '%d.%m.%Y %H.%M')
 
+
 def sort_events_by_date(events):
     return sorted(events, key=get_event_date)
+
 
 @app.route("/<lang>/events")
 @app.route('/events')
 def events(lang="fi"):
     # Fetch events from MongoDB
     events_data = events_collection.find()
-    
+
     # Filter out events with past dates
     current_datetime = datetime.now()
-    future_events = [event for event in events_data if get_event_date(event) >= current_datetime]
+    future_events = [event for event in events_data if get_event_date(
+        event) >= current_datetime]
     sorted_events = sort_events_by_date(future_events)
     return render_template(f'{lang}/events.html', title='Events', events=sorted_events, current_year=2023)
 
@@ -81,6 +82,7 @@ def event_details(lang="fi", event_id=None):
         pass
 
     return render_template(f'{lang}/event_details.html', event=event, current_year=2023)
+
 
 @app.route('/<lang>/signup/<event_id>', methods=["GET", "POST"])
 @app.route('/signup/<event_id>', methods=["GET", "POST"])
@@ -108,14 +110,16 @@ def event_signup(lang="fi", event_id=None):
 
         if lang == "fi":
             flash("Ilmoittautuminen onnistui!", "info")
-        
+
         if lang == "en":
             flash("Successfully registered!", "info")
-        return redirect(f'/{lang}/events')  # Redirect to events page after signup.
+        # Redirect to events page after signup.
+        return redirect(f'/{lang}/events')
 
     return render_template(f'{lang}/signup.html', event_id=event_id, event=event)
 
 # EVENTS END
+
 
 @app.route('/<lang>/about')
 @app.route('/about')
@@ -128,44 +132,75 @@ def about(lang="fi"):
 def contact(lang="fi"):
     if request.method == "GET":
         return render_template(f'{lang}/contact.html', title="Contact Us", current_year=2023)
-    
+
     if request.method == "POST":
         name = request.form.get("name")
         email = request.form.get("email")
         message = request.form.get("message")
         phonenumber = request.form.get("phonenumber")
-        
-        contactions_collection.insert_one({"name": name, "email": email, "message": message, "phonenumber": phonenumber})
+
+        contactions_collection.insert_one(
+            {"name": name, "email": email, "message": message, "phonenumber": phonenumber})
         return render_template(f'{lang}/contact.html', title="Contact Us", current_year=2023)
-    
+
+
 @app.route("/<lang>/join", methods=["GET", "POST"])
 @app.route('/join', methods=["GET", "POST"])
 def join(lang="fi"):
     if request.method == "GET":
         return render_template(f'{lang}/join_us.html', title="Join Us", current_year=2023)
-    
+
     if request.method == "POST":
         name = request.form.get("name")
         email = request.form.get("email")
         message = request.form.get("message")
         phonenumber = request.form.get("phonenumber")
         roles = request.form.getlist('roles')
-        
-        joins_collection.insert_one({"name": name, "email": email, "message": message, "phonenumber": phonenumber, "roles": roles})
+
+        joins_collection.insert_one(
+            {"name": name, "email": email, "message": message, "phonenumber": phonenumber, "roles": roles})
         return render_template(f'{lang}/join_us.html', title="Join Us", current_year=2023)
-    
+
+# TODO: #8 Clean this function
+def lang_thing(lang, path):
+    if lang == "fi":
+        path = path.replace("en", "fi")
+
+    if lang == "en":
+        path = path.split("/")
+
+        cont = False
+
+        def pop_unne(path, cont):
+            for i in range(0, len(path)):
+                if cont:
+                    continue
+
+                if ".org" in path[i]: # Our domain is sinimustaahallitustavastaan.ORG
+                    cont = True
+
+                path.pop(i)
+
+        pop_unne(path, cont)
+
+        def list_to_str(listi):
+            text = ""
+            for item in listi:
+                text += f"/{item}"
+
+            text = text.replace("//", "/")
+
+            return text
+
+        path = ("/en/" + list_to_str(path)).replace("//", "/")
+
+    return path
 
 @app.route('/change_language/<lang>')
 def change_language(lang):
-    if lang == "en":
-        path = request.referrer.split("/")[-1]
-        path = "/en/" + path
-
-    if lang == "fi":
-        path = request.referrer.replace("/en/", "/")
+    path = lang_thing(lang, request.referrer)
 
     return redirect(path)
-
 
 
 if __name__ == '__main__':
