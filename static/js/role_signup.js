a832299288cd3d318464123cbe638c3b25df5d3e
Verso <vuoreol@pm.me>
2023-10-22 23:59:19 +0300

    More efficient site (#73)
    * Added stuff
    
    * * now you can see the status bar updating as you sign up

--- static/js/role_signup.js
+++ static/js/role_signup.js
@@ -1,6 +1,89 @@
+// Add an event listener for role checkboxes
+document.querySelectorAll(".custom-checkbox").forEach(function (checkbox) {
+  checkbox.addEventListener("change", function () {
+    const roleContainer = checkbox.closest(".role");
+    const progressBar = roleContainer.querySelector(".progress-bar");
+    const roleCount = roleContainer.querySelector(".role-count");
+
+    const roleData = fix_role();
+
+    // Check if the checkbox was checked or unchecked
+    if (checkbox.checked) {
+      if (!roleData.checked) {
+        // If it wasn't checked before, increment the count
+        roleData.count += 1;
+        roleData.checked = true;
+      }
+    } else {
+      if (roleData.checked) {
+        // If it was checked before, decrement the count
+        roleData.count -= 1;
+        roleData.checked = false;
+      }
+      // Ensure the count doesn't go below 0
+      if (roleData.count < 0) {
+        roleData.count = 0;
+      }
+    }
+
+    // Update the role count and progress bar
+    roleCount.textContent = `Tällä hetkellä: ${roleData.count} / Tarvitaan vähintään: ${roleData.min_count}`;
+    progressBar.style.width = `${(roleData.count / roleData.min_count) * 100}%`;
+
+      function fix_role() {
+          const roleDataString = roleContainer.getAttribute("data-role");
+
+          // Remove single quotes inside double-quoted strings
+          const cleanedRoleDataString = roleDataString.replace(
+              /&quot;([^'&]+)&quot;/g,
+              (match, content) => `&quot;${content.replace(/'/g, "")}&quot;`
+          );
+          console.log(cleanedRoleDataString);
+
+          // Replace single quotes with double quotes for values or keys that don't start with "
+          const cleanedRoleDataString1 = cleanedRoleDataString.replace(
+              /("[^"]*")|'([^']+)'/g,
+              (match, doubleQuoted, singleQuoted) => {
+                  if (doubleQuoted) {
+                      return doubleQuoted; // Leave double-quoted parts unchanged
+                  } else if (singleQuoted) {
+                      return `"${singleQuoted.replace(/'/g, "")}"`; // Replace single quotes with double quotes
+                  }
+              }
+          );
+
+          console.log(cleanedRoleDataString1);
+
+          // Replace single quotes with double quotes for values or keys that don't start with " and ensure values don't start with "
+          const cleanedRoleDataString2 = cleanedRoleDataString1.replace(
+              /("[^"]*")|'([^']+)'/g,
+              (match, doubleQuoted, singleQuoted) => {
+                  if (doubleQuoted) {
+                      return doubleQuoted; // Leave double-quoted parts unchanged
+                  } else if (singleQuoted) {
+                      // Check if the single-quoted part starts with a double quote, and if it does, replace single quotes with double quotes
+                      if (singleQuoted.startsWith('"')) {
+                          return singleQuoted.replace(/'/g, "");
+                      } else {
+                          return `"${singleQuoted.replace(/'/g, "")}"`;
+                      }
+                  }
+              }
+          );
+
+          const final = cleanedRoleDataString2.replace("''", '""');
+
+          // Parse the JSON data
+          const roleData = JSON.parse(final);
+          console.log(roleData);
+          return roleData;
+      }
+  });
+});
+
 // Lisää klikkitapahtuman kuuntelija jokaiseen roolilaatikkoon
 document.querySelectorAll(".role").forEach(function (box) {
-  // Kun roolilaatikkoa klikataan, vaihda valintaruutu ja sovella tyylejä
+  // When a roolilaatikkoa is clicked, toggle the checkbox and apply styles
   box.addEventListener("click", function () {
     const checkbox = box.querySelector(".custom-checkbox");
     checkbox.checked = !checkbox.checked;
